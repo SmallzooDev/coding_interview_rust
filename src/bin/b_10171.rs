5ae4890636e87a5e13d3df726c1388be6004e953
smallzooDev <smallzooDev@gmail.com>
2025-05-19 13:57:41 +0900

    first commit

---
+++ src/bin/b_10171.rs
@@ -0,0 +1,13 @@
+// Baekjoon - 10171
+// https://www.acmicpc.net/problem/10171
+
+use std::io::{self, Write};
+
+fn main() {
+    let mut stdout = io::stdout().lock();
+
+    writeln!(stdout, "\\    /\\").unwrap();
+    writeln!(stdout, " )  ( ')").unwrap();
+    writeln!(stdout, "(  /  )").unwrap();
+    writeln!(stdout, " \\(__)|").unwrap();
+}
