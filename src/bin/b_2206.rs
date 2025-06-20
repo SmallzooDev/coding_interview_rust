2f2734ef9dc84537d2b264b09ebf63c2231047dc
smallzooDev <smallzooDev@gmail.com>
2025-06-20 17:08:14 +0900

    first commit

---
+++ src/bin/b_2206.rs
@@ -0,0 +1,185 @@
+// Baekjoon - 2206
+// https://www.acmicpc.net/problem/2206
+
+#[allow(clippy::all)]
+#[allow(unused_must_use, unused_doc_comments)]
+fn solve<R: BufRead, W: Write>(io: &mut IO<R, W>) -> Option<()> {
+    let [n, m] = io.get([0usize, 0usize])?;
+    let graph = io.get(vec![B; n])?;
+
+    io.put(bfs(&graph, n, m)).nl();
+    None
+}
+
+fn bfs(graph: &[Vec<u8>], n: usize, m: usize) -> i64 {
+    let mut visited = vec![vec![vec![0; 2]; m]; n];
+    let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+    let mut queue = VecDeque::new();
+
+    visited[0][0][0] = 1;
+
+    queue.push_back((0, 0, 1, 0));
+
+    while let Some((y, x, dist, broken)) = queue.pop_front() {
+        if y == n - 1 && x == m - 1 {
+            return dist;
+        }
+
+        for (dy, dx) in dirs {
+            let ny = y as i32 + dy;
+            let nx = x as i32 + dx;
+
+            if ny < 0 || ny >= n as i32 || nx < 0 || nx >= m as i32 {
+                continue;
+            }
+
+            let ny = ny as usize;
+            let nx = nx as usize;
+
+            if graph[ny][nx] == b'0' {
+                if visited[ny][nx][broken] == 0 {
+                    visited[ny][nx][broken] = 1;
+                    queue.push_back((ny, nx, dist + 1, broken));
+                }
+            } else if broken == 0 && visited[ny][nx][1] == 0 {
+                visited[ny][nx][1] = 1;
+                queue.push_back((ny, nx, dist + 1, 1));
+            }
+        }
+    }
+
+    -1
+}
+
+/// IO template - from bubbler (modified)
+// boj - https://www.acmicpc.net/user/bubbler
+#[allow(dead_code)]
+mod io {
+    pub(crate) use std::io::{stdin, stdout, BufRead, BufWriter, Write};
+    pub(crate) struct IO<R: BufRead, W: Write> {
+        ii: I<R>,
+        oo: BufWriter<W>,
+    }
+    impl<R: BufRead, W: Write> IO<R, W> {
+        pub(crate) fn new(r: R, w: W) -> Self {
+            Self {
+                ii: I::new(r),
+                oo: BufWriter::new(w),
+            }
+        }
+        pub(crate) fn get<T: Fill>(&mut self, exemplar: T) -> Option<T> {
+            self.ii.get(exemplar)
+        }
+
+        pub(crate) fn put<T: Print>(&mut self, t: T) -> &mut Self {
+            t.print(&mut self.oo);
+            self
+        }
+        pub(crate) fn nl(&mut self) -> &mut Self {
+            self.put("\n")
+        }
+    }
+    pub(crate) trait Print {
+        fn print<W: Write>(&self, w: &mut W);
+    }
+    macro_rules! print_disp {
+        ($($t:ty),+) => {
+            $(impl Print for $t { fn print < W : Write > (& self, w : & mut W) {
+            write!(w, "{}", self) .unwrap(); } })+
+        };
+    }
+    print_disp!(usize, i64, String, &str, char);
+    pub(crate) struct I<R: BufRead> {
+        r: R,
+        line: String,
+        rem: &'static str,
+    }
+    impl<R: BufRead> I<R> {
+        pub(crate) fn new(r: R) -> Self {
+            Self {
+                r,
+                line: String::new(),
+                rem: "",
+            }
+        }
+        pub(crate) fn next_line(&mut self) -> Option<()> {
+            self.line.clear();
+            (self.r.read_line(&mut self.line).unwrap() > 0).then(|| {
+                self.rem = unsafe { (&self.line[..] as *const str).as_ref().unwrap() };
+            })
+        }
+        pub(crate) fn get<T: Fill>(&mut self, exemplar: T) -> Option<T> {
+            let mut exemplar = exemplar;
+            exemplar.fill_from_input(self)?;
+            Some(exemplar)
+        }
+    }
+    pub(crate) trait Fill {
+        fn fill_from_input<R: BufRead>(&mut self, i: &mut I<R>) -> Option<()>;
+    }
+    fn ws(c: char) -> bool {
+        c <= ' '
+    }
+    macro_rules! fill_num {
+        ($($t:ty),+) => {
+            $(impl Fill for $t { fn fill_from_input < R : BufRead > (& mut self, i : &
+            mut I < R >) -> Option < () > { i.rem = i.rem.trim_start_matches(ws); while i
+            .rem.is_empty() { i.next_line() ?; i.rem = i.rem.trim_start_matches(ws); }
+            let tok = i.rem.split(ws).next().unwrap(); i.rem = & i.rem[tok.len()..]; *
+            self = tok.parse().ok() ?; Some(()) } })+
+        };
+    }
+    fill_num!(usize, i64, f64);
+    impl Fill for String {
+        fn fill_from_input<R: BufRead>(&mut self, i: &mut I<R>) -> Option<()> {
+            i.rem = i.rem.trim_start_matches(ws);
+            while i.rem.is_empty() {
+                i.next_line()?;
+                i.rem = i.rem.trim_start_matches(ws);
+            }
+            let tok = i.rem.split(ws).next().unwrap();
+            i.rem = &i.rem[tok.len()..];
+            *self = tok.to_string();
+            Some(())
+        }
+    }
+    impl Fill for Vec<u8> {
+        fn fill_from_input<R: BufRead>(&mut self, i: &mut I<R>) -> Option<()> {
+            i.rem = i.rem.trim_start_matches(ws);
+            while i.rem.is_empty() {
+                i.next_line()?;
+                i.rem = i.rem.trim_start_matches(ws);
+            }
+            let tok = i.rem.split(ws).next().unwrap();
+            i.rem = &i.rem[tok.len()..];
+            self.extend_from_slice(tok.as_bytes());
+            Some(())
+        }
+    }
+    pub(crate) const B: Vec<u8> = Vec::new();
+    impl<T: Fill> Fill for Vec<T> {
+        fn fill_from_input<R: BufRead>(&mut self, i: &mut I<R>) -> Option<()> {
+            for ii in self.iter_mut() {
+                ii.fill_from_input(i)?;
+            }
+            Some(())
+        }
+    }
+    impl<T: Fill, const N: usize> Fill for [T; N] {
+        fn fill_from_input<R: BufRead>(&mut self, i: &mut I<R>) -> Option<()> {
+            for ii in self.iter_mut() {
+                ii.fill_from_input(i)?;
+            }
+            Some(())
+        }
+    }
+}
+use std::{collections::VecDeque, vec};
+
+use io::*;
+pub fn main() {
+    let stdin = stdin().lock();
+    let stdout = stdout().lock();
+    let mut io = IO::new(stdin, stdout);
+    solve(&mut io);
+}
